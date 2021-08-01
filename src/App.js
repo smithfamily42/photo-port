58f6d2451f00f600be60c71cb00d2831f2ca814a
Brad Smith <smithfamily42@gmail.com>
2021-08-01 00:03:34 -0700

    conditional rendering and react hooks

--- src/App.js
+++ src/App.js
@@ -24,10 +24,8 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
-        <div>
-          <Gallery></Gallery>
+          <Gallery currentCategory={currentCategory}></Gallery>
           <About></About>
-        </div>
       </main>
     </div>
   );
